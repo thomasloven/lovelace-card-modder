5e556df3451a9a625479ce672d8b5872b6e644d6
Thomas Lovén <thomasloven@gmail.com>
2018-08-28 21:21:04 +0200

    Proof of concept... I guess...

---
+++ README.md
@@ -0,0 +1,15 @@
+```yaml
+      - type: custom:card-modder
+        card:
+          type: entities
+          entities:
+            - light.taklampa_kontoret
+            - light.taklampa_l_hall
+            - media_player.fk_hall_media
+        style:
+          "--paper-card-background-color": "red"
+          "--paper-item-icon-color": "yellow"
+          "color": "blue"
+          "background": "url(http://placekitten.com/100/100)"
+          height: 500px
+```
---
+++ card-modder.js
@@ -0,0 +1,39 @@
+class CardModder extends Polymer.Element {
+
+  setConfig(config) {
+    this._config = config;
+
+    let tag = config.card.type;
+    if(tag.startsWith("custom:"))
+      tag = tag.substr(7);
+    else
+      tag = `hui-${tag}-card`;
+
+    this.card = document.createElement(tag);
+    this.card.setConfig(config.card);
+    this.appendChild(this.card);
+  }
+
+  ready() {
+    super.ready();
+
+    this.card.ready();
+    let target = this.card;
+    if(this.card.shadowRoot && this.card.shadowRoot.querySelector("ha-card")) {
+      target = this.card.shadowRoot.querySelector("ha-card");
+    }
+    for(var k in this._config.style) {
+      target.style.setProperty(k, this._config.style[k]);
+    }
+  }
+
+  set hass(hass) {
+    this.card.hass = hass;
+  }
+
+  getCardSize() {
+    return this.card.getCardSize();
+  }
+}
+
+customElements.define('card-modder', CardModder);
